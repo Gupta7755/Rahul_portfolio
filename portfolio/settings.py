8e43d0a0174fff74f1e760837280d0d66568ee78
Gupta7755 <rahulkumarrpkkram7755@gmail.com>
2026-03-22 19:48:02 +0530

    Debug: Force DEBUG=True to see index 500 error traceback

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-for-local-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
